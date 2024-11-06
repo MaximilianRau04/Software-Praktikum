0fccf3c5f35f4231d221678d9fe8edc79ab04b65
Max Peresunchak <st152466@stud.uni-stuttgart.de>
2024-11-06 21:28:03 +0100

    Initial setup for backend with Spring Boot

---
+++ backend/src/main/java/com/sopra/eaplanner/EaplannerApplication.java
@@ -0,0 +1,13 @@
+package com.sopra.eaplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaplannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sopra/eaplanner/EaplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopra.eaplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
