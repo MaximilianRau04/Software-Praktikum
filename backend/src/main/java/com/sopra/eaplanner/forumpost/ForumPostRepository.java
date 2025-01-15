135f3df1575122ac98535631985d6fee217aab28
Max Peresunchak <st152466@stud.uni-stuttgart.de>
2025-01-15 18:10:05 +0100

    Remove duplicate code ForumPost

--- backend/src/main/java/com/sopra/eaplanner/forumpost/ForumPostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sopra.eaplanner.forumpost;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ForumPostRepository extends CrudRepository<ForumPost, Long> {
-}
