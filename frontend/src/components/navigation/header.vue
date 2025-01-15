3c9f6d436be573a4996220dc4a4c5f0dcd1ca1c0
Max Peresunchak <st152466@stud.uni-stuttgart.de>
2025-01-15 19:07:38 +0100

    Merge Trainerprofile and notification in header

--- frontend/src/components/navigation/header.vue
+++ frontend/src/components/navigation/header.vue
@@ -2,11 +2,20 @@
   <div class="header-container">
     <div class="header">
       <div class="sidebar-toggle" @click="handleToggleSidebar">
-        <img src="@/images/sidebar.png" alt="Sidebar Toggle" class="sidebar-icon" />
+        <img
+          src="@/images/sidebar.png"
+          alt="Sidebar Toggle"
+          class="sidebar-icon"
+        />
       </div>
 
       <div class="search-container">
-        <input type="text" v-model="value" class="search-input" placeholder="Suche..." />
+        <input
+          type="text"
+          v-model="value"
+          class="search-input"
+          placeholder="Suche..."
+        />
       </div>
 
       <div class="notification-container" @click="toggleNotificationMenu">
@@ -30,7 +39,9 @@
       </div>
 
       <div class="user-info">
-        <div class="user-label">{{ currentUser.username || 'Gast' }}</div>
+        <div class="user-label" @click="navigateToProfile">
+          {{ currentUser.username || "Gast" }}
+        </div>
       </div>
 
     </div>
@@ -38,10 +49,10 @@
 </template>
 
 <script>
-import { ref, computed, onMounted } from 'vue';
-import { globalState } from '@/types/User';
-import Cookies from 'js-cookie';
-import '../../assets/header.css';
+import { ref, onMounted } from "vue";
+import { globalState } from "@/types/User";
+import Cookies from "js-cookie";
+import "../../assets/header.css";
 import NotificationCard from '../notification/NotificationCard.vue';
 import config from "../../config";
 
@@ -88,19 +99,26 @@ export default {
       this.toggleSidebar();
     },
     handleLogin() {
-      this.$router.push('/login');
+      this.$router.push("/login");
     },
     logout() {
-      Cookies.remove('userId');
-      Cookies.remove('username');
-      Cookies.remove('firstname');
-      Cookies.remove('lastname');
-      Cookies.remove('role');
+      Cookies.remove("userId");
+      Cookies.remove("username");
+      Cookies.remove("firstname");
+      Cookies.remove("lastname");
+      Cookies.remove("role");
       globalState.setUser(null);
-      this.$router.push('/login');
+      this.$router.push("/login");
     },
     toggleToRegistration() {
-      this.$router.push('/register');
+      this.$router.push("/register");
+    },
+    navigateToProfile() {
+      if (this.currentUser.id) {
+        this.$router.push(`/user/${this.currentUser.username}`);
+      } else {
+        this.$router.push("/login");
+      }
     },
     toggleNotificationMenu() {
       this.showNotifications = !this.showNotifications;
@@ -109,15 +127,19 @@ export default {
       this.$emit('mark-as-read', notificationId);
     },
   },
-  mounted() {
-    const userId = Cookies.get('userId');
+
+  /**
+   * Check if user is already logged in
+   */
+  onMounted() {
+    const userId = Cookies.get("userId");
     if (userId) {
       const userData = {
         id: userId,
-        username: Cookies.get('username'),
-        firstname: Cookies.get('firstname'),
-        lastname: Cookies.get('lastname'),
-        role: Cookies.get('role'),
+        username: Cookies.get("username"),
+        firstname: Cookies.get("firstname"),
+        lastname: Cookies.get("lastname"),
+        role: Cookies.get("role"),
       };
       globalState.setUser(userData);
     }
